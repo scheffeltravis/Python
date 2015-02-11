d7813ca9bfbe14b22d6e144403efa6ba51d252cc
Travis Scheffel <travis.scheffel@gmail.com>
2015-02-11 09:08:52 -0600

    Benford's Law
    This program takes in census data from the input text document, and
    verify's Benford's law using a dictionary to account for the digits'
    frequencies.

---
+++ Benford's Law/Benford.py
@@ -0,0 +1,60 @@
+#  File: Benford.py
+
+#  Description: Takes in a given file of population data which is therefore
+#  analyzed using Benford's Law, and prints out the results.
+
+#  Student Name: Travis Scheffel
+
+#  Student UT EID: TWS 529
+
+#  Course Name: CS 303E
+
+#  Unique Number: 51630
+
+#  Date Created: 2/9/2015
+
+#  Date Last Modified: 2/10/2015
+
+def main():
+
+  # Open the given file to read input.
+  infile = open ("Census_2009.txt", "r")
+  pop_num = []
+  count = 0
+
+  # Runs through all lines of the given file and appends the values to a list.
+  for line in infile:
+    if (count == 0):
+      count += 1
+      continue
+    else:
+      count += 1
+      word_list = line.strip().split()
+      pop_num.append (word_list[-1])
+
+  # Define an empty dictionary to populate.
+  freq = {}
+
+  # Populate the library with the number of values which begin a certain digit.
+  for i in range(count - 1):
+    pop_num[i] = int(pop_num[i]) // (10 ** (len(pop_num[i]) - 1))
+    digit = pop_num[i]
+
+    if digit in freq:
+      freq[digit] = freq[digit] + 1
+    else:
+      freq[digit] = 1
+
+  # Print out the digits and their frequencies in the given format.
+  print ("Digit   Count     %")
+  for digit in freq:
+    percent = (freq[digit] / count) * 100
+    
+    print ("{0:<9}".format(digit) +
+           "{0:<9}".format(freq[digit]) +
+           "{0:.2f}".format(percent))
+
+  # Close the input file.
+  infile.close()
+
+main()
